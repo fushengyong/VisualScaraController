63e94936d04a9606a09e338d6bbc07f19b9d796e
radioactivated <gaowmhs1@gmail.com>
2016-10-31 19:07:30 -0400

    Serial.begin(9600)... that's all

--- arduino/scara_rxtx/scara_rxtx.ino
+++ arduino/scara_rxtx/scara_rxtx.ino
@@ -1,6 +1,9 @@
+// TODO: put required libraries in repo
+// or make a note that they're needed
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
