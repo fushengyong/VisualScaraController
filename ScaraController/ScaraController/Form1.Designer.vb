2ff857c3b5c2b0ff9e3e80896acb4fced773e0e7
radioactivated <gaowmhs1@gmail.com>
2016-10-31 18:21:47 -0400

    Add boilerplate for selecting and connecting to COM ports

--- ScaraController/ScaraController/Form1.Designer.vb
+++ ScaraController/ScaraController/Form1.Designer.vb
@@ -23,34 +23,70 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ActiveSerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.PortsListBox = New System.Windows.Forms.ListBox()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.StatusTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'SerialPort1
+        'ActiveSerialPort
         '
-        Me.SerialPort1.PortName = "COM4"
+        Me.ActiveSerialPort.PortName = "COM4"
         '
-        'ListBox1
+        'PortsListBox
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(46, 30)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(240, 316)
-        Me.ListBox1.TabIndex = 0
+        Me.PortsListBox.FormattingEnabled = True
+        Me.PortsListBox.Location = New System.Drawing.Point(46, 30)
+        Me.PortsListBox.Name = "PortsListBox"
+        Me.PortsListBox.Size = New System.Drawing.Size(198, 186)
+        Me.PortsListBox.TabIndex = 0
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(46, 248)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(197, 24)
+        Me.ConnectButton.TabIndex = 1
+        Me.ConnectButton.Text = "Attempt Connection"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'StatusTextBox
+        '
+        Me.StatusTextBox.Location = New System.Drawing.Point(102, 222)
+        Me.StatusTextBox.Name = "StatusTextBox"
+        Me.StatusTextBox.ReadOnly = True
+        Me.StatusTextBox.Size = New System.Drawing.Size(141, 20)
+        Me.StatusTextBox.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 225)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Status:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(716, 399)
-        Me.Controls.Add(Me.ListBox1)
+        Me.ClientSize = New System.Drawing.Size(858, 399)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StatusTextBox)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.PortsListBox)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ActiveSerialPort As IO.Ports.SerialPort
+    Friend WithEvents PortsListBox As ListBox
+    Friend WithEvents ConnectButton As Button
+    Friend WithEvents StatusTextBox As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- ScaraController/ScaraController/Form1.vb
+++ ScaraController/ScaraController/Form1.vb
@@ -1,7 +1,25 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         For Each sp As String In My.Computer.Ports.SerialPortNames
-            ListBox1.Items.Add(sp)
+            PortsListBox.Items.Add(sp)
         Next
+    End Sub
+
+    Private Sub TransmitString(str As String)
+        ' treat str as a "register" of bytes
+        ' 
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortsListBox.SelectedIndexChanged
+        ActiveSerialPort.PortName = PortsListBox.Items(PortsListBox.SelectedIndex)
+        'MsgBox(ActiveSerialPort.PortName)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        If PortsListBox.SelectedIndex = -1 Then
+            MsgBox("Please select a COM port.", MsgBoxStyle.Exclamation, "No port selected")
+        Else
+
+        End If
     End Sub
 End Class
