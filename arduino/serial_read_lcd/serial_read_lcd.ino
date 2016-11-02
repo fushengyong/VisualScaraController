d1746ff29b872e65e72b6b14249deaa54f7a9007
radioactivated <gaowmhs1@gmail.com>
2016-11-02 17:38:00 -0400

    Put in modified code from arduino website to read serial

--- arduino/serial_read_lcd/serial_read_lcd.ino
+++ arduino/serial_read_lcd/serial_read_lcd.ino
@@ -1,14 +1,23 @@
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(4,5,6,7,8,9,10,11,12,13);
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   lcd.begin(16,2);
   lcd.print("hello world!");
-  delay(4000);
+  delay(3000);
+  lcd.clear();
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    lcd.print(Serial.readString());
+  }
 }
