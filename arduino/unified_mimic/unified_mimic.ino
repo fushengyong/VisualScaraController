c9718e752dfa0553f5d082927d288d79c768ee52
radioactivated <gaowmhs1@gmail.com>
2016-11-27 21:30:01 -0500

    Add defines for a bunch of pin numbers

--- arduino/unified_mimic/unified_mimic.ino
+++ arduino/unified_mimic/unified_mimic.ino
@@ -18,33 +18,50 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include <AccelStepper.h>
 #include <MultiStepper.h>
-
-#include <SparkFunAutoDriver.h>
-#include <SparkFundSPINConstants.h>
-
 #include <Servo.h>
 
-// mimic controller setup
+// analog inputs
+#define BASE_POT A0
+#define ELBOW_POT A1
+#define ZAXIS_POT A2
 
-// potentiometer definitions
-int BasePot = A0;
-int ElbowPot = A1;
-int ZAxisPot = A2; // linear potentiometer
+// digital inputs
+#define UNFOLD_PIN 44
+#define FLIP_PIN 45 // still figuring this function out
+#define LED_PIN 46
+#define DISPENSE_PIN 47 // for a solenoid?0
 
-// digital input definitions
-int unfoldPin = 44;
-int vacuumPin = 45;
-int effectorPin1 = 46; // determines orientation/configuration of end effector
+// hinge stepper - hooked up to external L298N board0
+#define HINGE_1 24
+#define HINGE_2 26
+#define HINGE_3 28
+#define HINGE_4 30
 
+// base stepper - hooked up to arduino motor shield r3
+#define BASE_EN1 3 // must be set HIGH
+#define BASE_EN2 11 // must be set HIGH
+#define BASE_1 25 
+#define BASE_2 27
+#define BASE_3 29
+#define BASE_4 31
+
+#define ZAXIS_STEP 10
+#define ZAXIS_DIR 7
+#define ZAXIS_EN 6
+
+#define ELBOW_STEP 5
+#define ELBOW_DIR 4
+#define ELBOW_EN 2
 // stepper motors
-AccelStepper HingeStepper(AccelStepper::FULL4WIRE,24,26,28,30,true); // l298n can be run as a full 4-wire mode
+AccelStepper HingeStepper(AccelStepper::FULL4WIRE,HINGE_1,HINGE_2,HINGE_3,HINGE_4,true); // l298n can be run as a full 4-wire mode
+AccelStepper BaseStepper(AccelStepper::FULL4WIRE,BASE_1,BASE_2,BASE_3,BASE_4,true);
 // the only thing that EasyDrivers need bare minimum is STEP and DIR
-AccelStepper ZAxisStepper(AccelStepper::DRIVER,3,2,true)
-AccelStepper ElbowStepper(AccelStepper::DRIVER,5,4,true)
+AccelStepper ZAxisStepper(AccelStepper::DRIVER,ZAXIS_STEP,ZAXIS_DIR,true) // EN must still be connected
+AccelStepper ElbowStepper(AccelStepper::DRIVER,ELBOW_STEP,ELBOW_DIR,true)
 
 // servos
-Servo effServ1;
-Servo effServ2;
+Servo eServo1;
+Servo eServo2;
 
 void setup() {
   // put your setup code here, to run once:
