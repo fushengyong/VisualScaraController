dd12a9f2e13eea31ffb018e67e6ec828a16998dc
radioactivated <gaowmhs1@gmail.com>
2016-12-25 21:23:36 -0500

    Update to mimic controller code

--- arduino/unified_mimic/unified_mimic.ino
+++ arduino/unified_mimic/unified_mimic.ino
@@ -21,15 +21,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <Servo.h>
 
 // analog inputs
-#define BASE_POT A0
-#define ELBOW_POT A1
-#define ZAXIS_POT A2
+#define BASE_POT A8
+#define ELBOW_POT A9
+#define ZAXIS_POT A10
 
 // digital inputs
 #define UNFOLD_PIN 44
 #define FLIP_PIN 45 // still figuring this function out
 #define LED_PIN 46
 #define DISPENSE_PIN 47 // for a solenoid?0
+
+// digital outputs
+#define SOL_OUT 48
+#define LED_OUT 49
 
 // hinge stepper - hooked up to external L298N board0
 #define HINGE_1 24
@@ -40,35 +44,163 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // base stepper - hooked up to arduino motor shield r3
 #define BASE_EN1 3 // must be set HIGH
 #define BASE_EN2 11 // must be set HIGH
-#define BASE_1 25 
-#define BASE_2 27
-#define BASE_3 29
-#define BASE_4 31
+#define BASE_BRAKE1 8
+#define BASE_BRAKE2 9
+#define BASE_1 12
+#define BASE_2 13
 
+// regular EasyDriver
 #define ZAXIS_STEP 10
 #define ZAXIS_DIR 7
 #define ZAXIS_EN 6
-
+// Big EasyDriver
 #define ELBOW_STEP 5
 #define ELBOW_DIR 4
 #define ELBOW_EN 2
+
+// servo control pins
+#define CLAW_SIG 14
+#define FLIP_SIG 15
+
+#define BASE_STEPS 200
+#define ELBOW_STEPS 200
+#define ZAXIS_STEPS 200
+#define HINGE_STEPS 200
+#define DEADZONE 5
+
 // stepper motors
-AccelStepper HingeStepper(AccelStepper::FULL4WIRE,HINGE_1,HINGE_2,HINGE_3,HINGE_4,true); // l298n can be run as a full 4-wire mode
-AccelStepper BaseStepper(AccelStepper::FULL4WIRE,BASE_1,BASE_2,BASE_3,BASE_4,true);
+AccelStepper HingeStepper(AccelStepper::FULL4WIRE,HINGE_1,HINGE_2,HINGE_3,HINGE_4); // l298n can be run as a full 4-wire mode
+AccelStepper BaseStepper(AccelStepper::FULL2WIRE,BASE_1,BASE_2);
 // the only thing that EasyDrivers need bare minimum is STEP and DIR
-AccelStepper ZAxisStepper(AccelStepper::DRIVER,ZAXIS_STEP,ZAXIS_DIR,true) // EN must still be connected
-AccelStepper ElbowStepper(AccelStepper::DRIVER,ELBOW_STEP,ELBOW_DIR,true)
+AccelStepper ZAxisStepper(AccelStepper::DRIVER,ZAXIS_STEP,ZAXIS_DIR,true); // EN must still be connected
+AccelStepper ElbowStepper(AccelStepper::DRIVER,ELBOW_STEP,ELBOW_DIR,true);
 
 // servos
-Servo eServo1;
-Servo eServo2;
+Servo flipServo;
+Servo clawServo;
+
+// helps with controller deadzone
+int basePrevious = 0;
+int elbowPrevious = 0;
+int zAxisPrevious = 0;
+
+// other global vars
+bool ledOn = false;
+unsigned char flipPos = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  // set direction of all the pins
+  pinMode(UNFOLD_PIN,INPUT);
+  pinMode(FLIP_PIN,INPUT);
+  pinMode(LED_PIN,INPUT);
+  pinMode(DISPENSE_PIN,INPUT);
+
+  pinMode(LED_OUT,OUTPUT);
+  pinMode(SOL_OUT,OUTPUT);
+
+  // enable all the motors
+  pinMode(BASE_EN1,OUTPUT); 
+  digitalWrite(BASE_EN1,HIGH);
+  pinMode(BASE_EN2,OUTPUT);
+  digitalWrite(BASE_EN2,HIGH);
+  pinMode(BASE_BRAKE1,OUTPUT);
+  digitalWrite(BASE_BRAKE1,LOW);
+  pinMode(BASE_BRAKE2,OUTPUT);
+  digitalWrite(BASE_BRAKE2,LOW);
+
+  ZAxisStepper.setEnablePin(ZAXIS_EN);
+  ElbowStepper.setEnablePin(ELBOW_EN);
+
+  // set stepper max speeds
+  BaseStepper.setMaxSpeed(1000);
+  ElbowStepper.setMaxSpeed(1000);
+  HingeStepper.setMaxSpeed(1000);
+  ZAxisStepper.setMaxSpeed(1000);
+  
+  
+  // attach servos
+  flipServo.attach(FLIP_SIG);
+  clawServo.attach(CLAW_SIG);
+  
   // wait for unfold command
+  while(digitalRead(UNFOLD_PIN) == LOW);
+
+  // unfold when told to do so
+  HingeStepper.moveTo(90); // CHECK THE DIReCTION ON THiS oNE
+  HingeStepper.setSpeed(100);
+  HingeStepper.runSpeedToPosition();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // take inputs
+  
+  // read digital inputs
+  bool led = digitalRead(LED_PIN);
+  bool flip = digitalRead(FLIP_PIN);
+  bool dispense = digitalRead(DISPENSE_PIN);
+  
+  // read potentiometers
+  int baseReading = analogRead(BASE_POT);
+  int elbowReading = analogRead(ELBOW_POT);
+  int zAxisReading = analogRead(ZAXIS_POT);
+  // move motors while applying the deadzone
+  if(abs(baseReading - basePrevious) > DEADZONE) {
+    BaseStepper.moveTo(baseReading/16);
+
+  }
+  BaseStepper.setSpeed(100);
+  BaseStepper.runSpeedToPosition();
+  if(elbowReading - elbowPrevious > DEADZONE) {
+    ElbowStepper.moveTo(elbowReading/16);
+  }
+  ElbowStepper.setSpeed(100);
+  ElbowStepper.runSpeedToPosition();
+  if(zAxisReading - zAxisPrevious > DEADZONE) {
+    ZAxisStepper.moveTo(zAxisReading/16);
+  }
+  ZAxisStepper.setSpeed(100);
+  ZAxisStepper.runSpeedToPosition();
+
+  // respond to inputs
+  // digital outputs
+  if(led){
+    ledOn = !ledOn;
+    digitalWrite(LED_PIN,ledOn);
+  }
+
+  if(flip){
+    unsigned char sPos = flipPos;
+    if(sPos == 0){
+      sPos = 2100; // TODO: must figure this out by testing hardware
+    }
+    else {
+      sPos = 900; // TODO: this is currently set for Hitec servos, not generic futaba ones
+    }
+    flipServo.writeMicroseconds(sPos);
+  }
+
+  if(dispense){
+    digitalWrite(SOL_OUT,HIGH);
+  }else{
+    digitalWrite(SOL_OUT,LOW);
+  }
+  
+}
+
+
+float getAngleFromSteps(int steps, int stepsPerRot) { // return the angle to which a given number of steps corresponds
+  float rotations = (float)steps / stepsPerRot;
+  float angle = rotations * 360;
+  return angle;
+}
+
+int getStepsFromAngle(float angle, int stepsPerRot) { // return the number of steps needed to traverse a given angle
+  float rotations = (float)angle / 360;
+  long steps = stepsPerRot * rotations;
+  return steps;
 }
